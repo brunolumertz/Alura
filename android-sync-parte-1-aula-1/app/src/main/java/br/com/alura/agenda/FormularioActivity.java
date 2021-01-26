1e422aaa94aa2f393bc1bd43db05abc22fa7f3fa
Bruno <bruno14.lumertz@hotmail.com>
2021-01-26 14:21:28 -0300

    Revert "Web service 2"
    This reverts commit 316115d6f77553e38377729a337bcb09c1e09de9.

--- android-sync-parte-1-aula-1/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ android-sync-parte-1-aula-1/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -2,15 +2,19 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
--- android-sync-parte-1-aula-1/app/src/main/java/br/com/alura/agenda/dto/AlunoSync.java
+++ android-sync-parte-1-aula-1/app/src/main/java/br/com/alura/agenda/dto/AlunoSync.java
@@ -1,44 +1,4 @@
-package br.com.alura.agenda.dto;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.joda.time.LocalDateTime;
-
-import br.com.caelum.alura.model.Aluno;
+package br.com.alura.agenda;
 
 public class AlunoSync {
-
-    private final List<Aluno> alunos;
-    private final String momentoDaUltimaModificacao;
-
-    public AlunoSync(List<Aluno> alunos) {
-        this.alunos = alunos;
-        this.momentoDaUltimaModificacao = LocalDateTime.now().toString();
-    }
-
-    public AlunoSync(Aluno aluno) {
-        this.alunos = new ArrayList<>(Arrays.asList(aluno));
-        this.momentoDaUltimaModificacao = LocalDateTime.now().toString();
-    }
-
-    public AlunoSync(List<Aluno> alunos, String datahora) {
-        this.alunos = alunos;
-        this.momentoDaUltimaModificacao = datahora;
-    }
-
-    public AlunoSync(Aluno aluno, String datahora) {
-        this.alunos = new ArrayList<>(Arrays.asList(aluno));
-        this.momentoDaUltimaModificacao = datahora;
-    }
-
-    public List<Aluno> getAlunos() {
-        return alunos;
-    }
-
-    public String getMomentoDaUltimaModificacao() {
-        return momentoDaUltimaModificacao;
-    }
-
 }
