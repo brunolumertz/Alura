ff942ad2494e33f75e58c2682465fe1a21d8cb52
Bruno <bruno14.lumertz@hotmail.com>
2021-01-27 13:06:07 -0300

    Web 3
    Introdução ao versionamento

---
+++ android-sync-parte-3-projeto-inicial/app/src/main/java/br/com/alura/agenda/sinc/AlunoSincronizador.java
@@ -0,0 +1,74 @@
+package br.com.alura.agenda.sinc;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import org.greenrobot.eventbus.EventBus;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.dto.AlunoSync;
+import br.com.alura.agenda.event.AtualizaListaAlunoEvent;
+import br.com.alura.agenda.preferences.AlunoPreferences;
+import br.com.alura.agenda.retrofit.RetrofitInicializador;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AlunoSincronizador {
+    private final Context context;
+    private EventBus bus = EventBus.getDefault();
+    private AlunoPreferences preferences;
+
+    public AlunoSincronizador(Context context) {
+        this.context = context;
+        preferences = new AlunoPreferences(context);
+    }
+
+    public void buscaTodos(){
+        if (preferences.temVersao()){
+            buscaNovos();
+        }else {
+            buscaAlunos();
+        }
+    }
+
+    private void buscaNovos() {
+        String versao = preferences.getVersao();
+        Call<AlunoSync> call = new RetrofitInicializador().getAlunoService().novos(versao);
+        call.enqueue(buscaAlunosCallback());
+    }
+
+    private void buscaAlunos() {
+        Call<AlunoSync> call = new RetrofitInicializador().getAlunoService().lista();
+
+        call.enqueue(buscaAlunosCallback());
+    }
+
+    @NonNull
+    private Callback<AlunoSync> buscaAlunosCallback() {
+        return new Callback<AlunoSync>() {
+            @Override
+            public void onResponse(Call<AlunoSync> call, Response<AlunoSync> response) {
+                AlunoSync alunoSync = response.body();
+                String versao = alunoSync.getMomentoDaUltimaModificacao();
+
+                preferences.salvaVersao(versao);
+
+                AlunoDAO dao = new AlunoDAO(context);
+                dao.sincroniza(alunoSync.getAlunos());
+                dao.close();
+
+                Log.i("versao", preferences.getVersao());
+
+                bus.post(new AtualizaListaAlunoEvent());
+            }
+
+            @Override
+            public void onFailure(Call<AlunoSync> call, Throwable t) {
+                Log.e("onFailure chamado", t.getMessage());
+                bus.post(new AtualizaListaAlunoEvent());
+            }
+        };
+    }
+}
