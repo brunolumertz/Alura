c43c5e4becf9076451010986e612131e4c34a7ec
Bruno <bruno14.lumertz@hotmail.com>
2021-02-03 12:53:31 -0300

    kotlin 2
    Configurando botoes de ação

--- FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/activity/ListaTransacoesActivity.kt
+++ FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/activity/ListaTransacoesActivity.kt
@@ -1,19 +1,21 @@
-package br.com.alura.financask.ui.activity
+package br.com.alura.financask.ui.activity.activity
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
+import br.com.alura.financask.ui.activity.ResumoView
 import br.com.alura.financask.ui.activity.adapter.ListaTransacoesAdapter
-import br.com.alura.financask.ui.activity.extension.formataParaBrasileiro
+import br.com.alura.financask.ui.activity.delegate.TransacaoDelegate
+import br.com.alura.financask.ui.activity.dialog.AdicionaTransacaoDialog
 import br.com.alura.financask.ui.activity.model.Tipo
 import br.com.alura.financask.ui.activity.model.Transacao
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
-import kotlinx.android.synthetic.main.resumo_card.*
-import java.math.BigDecimal
-import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
+    private val transacoes: MutableList<Transacao> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
@@ -21,36 +23,49 @@ class ListaTransacoesActivity : AppCompatActivity() {
         // o var é mutavel, ou seja, usado quando quiser usar mais de um valor para a lista ou adicionar. o val é usado praticamente como uma static, não é alterada
         //o kotlim adiciona sozinho o tipo de lista, a nao ser que vc defina para ele. Exemplo: lista de string -->  var lista: List<String> = listof( "coisa em string")
 
-        val transacoes: List<Transacao> = transacoesDeExemplo()
+        configuraResumo()
+        configuraLista()
+        configuraFab()
+    }
 
-        ResumoView().adicionaReceitaNoResumo(transacoes)
+    private fun configuraFab() {
+        lista_transacoes_adiciona_receita
+            .setOnClickListener {
+                chamaDialogDeAdicao(Tipo.RECEITA)
+            }
+        lista_transacoes_adiciona_despesa
+            .setOnClickListener {
+                chamaDialogDeAdicao(Tipo.DESPESA)
+            }
+    }
+
+    private fun chamaDialogDeAdicao(tipo: Tipo) {
+        AdicionaTransacaoDialog(window.decorView as ViewGroup, this)
+            .chama(tipo, object : TransacaoDelegate {
+                override fun delegate(transacao: Transacao) {
+                    atualizaTransacoes(transacao)
+                    lista_transacoes_adiciona_menu.close(true)
+                }
+            })
     }
 
 
+    private fun atualizaTransacoes(transacao: Transacao) {
+        transacoes.add(transacao)
+        configuraLista()
+        configuraResumo()
+    }
 
-    private fun configuraLista(transacoes: List<Transacao>) {
+    private fun configuraResumo() {
+        val view: View = window.decorView
+        val resumoView = ResumoView(this, view, transacoes)
+        resumoView.atualiza()
+    }
+
+
+    private fun configuraLista() {
         lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
     }
 
-    private fun transacoesDeExemplo() = listOf(
-        Transacao(
-            tipo = Tipo.DESPESA,
-            data = Calendar.getInstance(),
-            valor = BigDecimal(20.5)
-        ),
-        Transacao(
-            valor = BigDecimal(100.0),
-            tipo = Tipo.RECEITA,
-            categoria = "Economia"
-        ),
-        Transacao(
-            valor = BigDecimal(200.0),
-            tipo = Tipo.DESPESA
-        ),
-        Transacao(
-            valor = BigDecimal(500.0),
-            categoria = "Prêmio",
-            tipo = Tipo.RECEITA
-        )
-    )
+
 }
