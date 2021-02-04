8ca76f3c95619a07fea0c4c1a998d88dbef3ddb6
Bruno <bruno14.lumertz@hotmail.com>
2021-02-04 12:34:00 -0300

    Finalização kotlin 3
    Alteracao de transacoes; Seguranca com null safety; Tecnicas de inicializacao; Flexibilidade com herenca; Higher Order Functions; Menu de contexto; Companion objects

--- FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/activity/ListaTransacoesActivity.kt
+++ FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/activity/ListaTransacoesActivity.kt
@@ -1,27 +1,41 @@
 package br.com.alura.financask.ui.activity.activity
 
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
 import br.com.alura.financask.ui.activity.ResumoView
 import br.com.alura.financask.ui.activity.adapter.ListaTransacoesAdapter
-import br.com.alura.financask.ui.activity.delegate.TransacaoDelegate
+import br.com.alura.financask.ui.activity.dao.TransacaoDAO
 import br.com.alura.financask.ui.activity.dialog.AdicionaTransacaoDialog
+import br.com.alura.financask.ui.activity.dialog.AlteraTransacaoDialog
 import br.com.alura.financask.ui.activity.model.Tipo
 import br.com.alura.financask.ui.activity.model.Transacao
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
-    private val transacoes: MutableList<Transacao> = mutableListOf()
+    private val dao = TransacaoDAO()
+    private val transacoes = dao.transacoes
+    private val viewDaActivity by lazy {
+        window.decorView
+    }
+
+    private val viewGroupDaActivity by lazy {
+        viewDaActivity as ViewGroup
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
         // o var é mutavel, ou seja, usado quando quiser usar mais de um valor para a lista ou adicionar. o val é usado praticamente como uma static, não é alterada
         //o kotlim adiciona sozinho o tipo de lista, a nao ser que vc defina para ele. Exemplo: lista de string -->  var lista: List<String> = listof( "coisa em string")
+
+        //   viewDaActivity = window.decorView
 
         configuraResumo()
         configuraLista()
@@ -40,31 +54,74 @@ class ListaTransacoesActivity : AppCompatActivity() {
     }
 
     private fun chamaDialogDeAdicao(tipo: Tipo) {
-        AdicionaTransacaoDialog(window.decorView as ViewGroup, this)
-            .chama(tipo, object : TransacaoDelegate {
-                override fun delegate(transacao: Transacao) {
-                    atualizaTransacoes(transacao)
-                    lista_transacoes_adiciona_menu.close(true)
-                }
-            })
+        AdicionaTransacaoDialog(viewGroupDaActivity, this)
+            .chama(tipo) { transaoCriada ->
+                adiciona(transaoCriada)
+                lista_transacoes_adiciona_menu.close(true)
+            }
+
     }
 
 
-    private fun atualizaTransacoes(transacao: Transacao) {
-        transacoes.add(transacao)
+    private fun adiciona(transacao: Transacao) {
+        dao.adiciona(transacao)
+        atualizaTransacoes()
+    }
+
+
+    private fun atualizaTransacoes() {
         configuraLista()
         configuraResumo()
     }
 
     private fun configuraResumo() {
-        val view: View = window.decorView
-        val resumoView = ResumoView(this, view, transacoes)
+        val resumoView = ResumoView(this, viewDaActivity, transacoes)
         resumoView.atualiza()
     }
 
 
     private fun configuraLista() {
-        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
+        val listaTransacoesAdapter = ListaTransacoesAdapter(transacoes, this)
+        with(lista_transacoes_listview) {
+            adapter = listaTransacoesAdapter
+            setOnItemClickListener { _, _, position, _ ->
+                val transacao = transacoes[position]
+                chamaDialogDeAlteracao(transacao, position)
+            }
+            setOnCreateContextMenuListener { menu, _, _ ->
+                menu.add(Menu.NONE, 1, Menu.NONE, "Remover")
+            }
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        val idDoMenu = item?.itemId
+        if (idDoMenu == 1){
+            val adapterMenuInfo = item.menuInfo as AdapterView.AdapterContextMenuInfo
+            val posicaoDaTransacao = adapterMenuInfo.position
+            remove(posicaoDaTransacao)
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    private fun remove(posicao: Int) {
+        dao.remove(posicao)
+        atualizaTransacoes()
+    }
+
+    private fun chamaDialogDeAlteracao(
+        transacao: Transacao,
+        position: Int
+    ) {
+        AlteraTransacaoDialog(viewGroupDaActivity, this)
+            .chama(transacao) { transaoAlterada ->
+                altera(transaoAlterada, position)
+            }
+    }
+
+    private fun altera(transacao: Transacao, position: Int) {
+        dao.altera(transacao, position)
+        atualizaTransacoes()
     }
 
 
